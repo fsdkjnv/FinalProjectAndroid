8e71ecf203c2e4df0cae3e6406bef9e7d4a2ce4e
vovi01012003 <152761814+vovi01012003@users.noreply.github.com>
2023-12-03 11:32:32 +0700

    Update DataManager.java
    Vi demo

--- app/src/main/java/com/example/drawer/fragment/DataManager.java
+++ app/src/main/java/com/example/drawer/fragment/DataManager.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import com.example.drawer.DataDevice;
 // ulatroihahaha
-
+//sualan1
 public class DataManager {
     private static DataManager instance;
     private Map<String, List<DataDevice>> roomData;
