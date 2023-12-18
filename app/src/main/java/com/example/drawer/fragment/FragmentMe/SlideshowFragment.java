b2eb0a8efba6308ea3b61580f2f831ca7dd64ab9
vothithuyvi <vovilak10202002@gmail.com>
2023-12-19 00:00:17 +0700

    Vi commit

--- app/src/main/java/com/example/drawer/fragment/FragmentMe/SlideshowFragment.java
+++ app/src/main/java/com/example/drawer/fragment/FragmentMe/SlideshowFragment.java
@@ -8,25 +8,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.drawer.Data.MyDataSingleton;
 import com.example.drawer.LoginandSignup;
 import com.example.drawer.R;
 
 public class SlideshowFragment extends Fragment {
     private Button btn_logout;
     private SharedPreferences sharedPreferences;
+    private TextView email; // Declare TextView
+    String userEmail = MyDataSingleton.getInstance().getUserEmail();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sildeshow, container, false);
+        email = view.findViewById(R.id.email_textview); // Initialize TextView
 
         btn_logout = view.findViewById(R.id.logout_button);
         sharedPreferences = getActivity().getSharedPreferences("loginPrefs", Context.MODE_PRIVATE);
+        email.setText(userEmail);
 
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
